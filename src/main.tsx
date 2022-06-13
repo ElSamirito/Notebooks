eb3fc009d60a4d72a9a6b2af6338ce42bd474d92
SamoCA15 <arabsamir2003@gmail.com>
2022-06-13 11:56:45 -0300

    Json & Son

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import {enableMapSet} from 'immer'
 import { useImmer } from 'use-immer'
 import './index.css'
 import './cssreset.css'
+import Compus from './Compus.json'
+import Profes from './Profes.json'
 
 // MMNNN**
 // *: vacio
@@ -15,16 +17,7 @@ import './cssreset.css'
 
 enableMapSet()
 
-var armario:Record<string, string>={
-    "7798162833996" : "Lenovo 01",
-    "7798298950147" : "Lenovo 48",
-    "4795864667808" : "CA 01",
-    "201807X05473" : "CA 15",
-    "6950126122336" : "Conectar 01",
-    "AA0811010109" : "Conectar 08"
-}
-
-function App() {
+function Registro() {
     const [codigo, setCodigo] = useState("")
     const [codigos, setCodigos] = useImmer(new Set<string>())
 
@@ -45,15 +38,15 @@ function App() {
     return (
         <>
             <form onSubmit={submit}>
-            <input className="barcode-input" value={codigo} onChange={e => setCodigo(e.target.value)} />
+            <input className="barcode-input" autoFocus value={codigo} onChange={e => setCodigo(e.target.value)} />
             </form>
             <ul className="notebook-list">
-                {[...codigos].map(c => <li>{armario[c]}</li>)}
+                {[...codigos].map(c => <li>{Compus[c as keyof typeof Compus]}</li>)}
             </ul>
         </>
     )
 }
 
 const root = createRoot(document.getElementById('root')!)
-root.render(<App />)
+root.render(<Registro/>)
 
