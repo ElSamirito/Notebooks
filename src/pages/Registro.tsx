b8fe47a3adcc75381421b8928a17aee4a07987c6
Santiago Cézar <santiagocezar2013@gmail.com>
2022-06-13 13:40:15 -0300

    saludos

--- src/pages/Registro.tsx
+++ src/pages/Registro.tsx
@@ -1,11 +1,14 @@
 import { FormEvent, useCallback, useState } from "react"
 import { useImmer } from "use-immer"
 import Compus from 'data/Compus.json'
+import Profes from 'data/Profes.json'
+import saludos from 'data/saludos.json'
 
 export function Registro() {
     const [codigo, setCodigo] = useState("")
-    const [codigos, setCodigos] = useImmer(new Set<string>())
-
+    const [saludo,] = useState(() => saludos[Math.floor(Math.random() * saludos.length)])
+    const [codigos, setCodigos] = useImmer(new Set<string>());
+    
     const submit = useCallback((e: FormEvent) => {
         e.preventDefault()
         setCodigo("")
@@ -22,8 +25,9 @@ export function Registro() {
 
     return (
         <>
+            <p className="saludo">{saludo.replace('{}', 'Jose Drueta')}</p>
             <form onSubmit={submit}>
-            <input className="barcode-input" autoFocus value={codigo} onChange={e => setCodigo(e.target.value)} />
+                <input placeholder="Código" className="barcode-input" autoFocus value={codigo} onChange={e => setCodigo(e.target.value)} />
             </form>
             <ul className="notebook-list">
                 {[...codigos].map(c => <li>{Compus[c as keyof typeof Compus]}</li>)}
