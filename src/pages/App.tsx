a346ca73e57cbcefba22412b5763c9bf68c8cb0b
Santiago Cézar <santiagocezar2013@gmail.com>
2022-06-13 12:37:14 -0300

    ruter

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,21 @@
-import { Registro } from "./Registro";
+import { Registro } from './Registro';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { Inicio } from './Inicio';
 
 export function App() {
-    return <Registro/>
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/">
+                    <Route index element={<Inicio/>}/>
+                    <Route path="profes"> 
+                        <Route index /* lista profes *//>
+                        <Route path=":profe" /* compus usando el profe *//>
+                    </Route>
+                    <Route path="registro" element={<Registro/>}/>
+                    <Route path="compus" /* compus sin devolver *//>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
